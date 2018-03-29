915b342fb7f34654ec23dc7a5064b16d51101b32
Gaurav <graheja1997@gmail.com>
2018-03-29 16:45:07 +0530

    Session fixed

--- src/app.py
+++ src/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 import os
 from src.common.database import Database
 
+import src.config as Config
 
 app = Flask(__name__)
 
-app.config.from_object('config')
+app.config.from_object(Config)
 app.secret_key = os.environ.get("app_secret_key")
 
 
