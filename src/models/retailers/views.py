10e18f49a70bf45a85a58e90fd8d72af1b002950
Dimple <goyaldimple75@gmail.com>
2018-03-31 03:28:00 +0530

    Changed data

--- src/models/retailers/views.py
+++ src/models/retailers/views.py
@@ -52,11 +52,12 @@ def sim_register(user_id):
     tsp = request.form['tsp']
     issue_date = datetime.datetime.now().strftime("%Y-%m-%d")
     lsa = request.form['lsa']
+    return "user registerd"
 
-    sim = Sim(aadhaar_no, mobile_no, tsp, issue_date, lsa)
-    if sim.save_to_db():
-        if requests.post("https://beast-cdb.herokuapp.com/api/tsp/",{"aadhaar_no":sim.aadhaar_no, "mobile_no":sim.mobile_no, "tsp":"Airtel", "issue_date":sim.issue_date,"lsa":sim.local_service_area}):
-            flash("Successfuly Registered")
-            return redirect(url_for('.dashboard',user_id =session['uid']))
-
-    flash("Registration failed")
+    # sim = Sim(aadhaar_no, mobile_no, tsp, issue_date, lsa)
+    # if sim.save_to_db():
+    #     if requests.post("https://beast-cdb.herokuapp.com/api/tsp/",{"aadhaar_no":sim.aadhaar_no, "mobile_no":sim.mobile_no, "tsp":"Airtel", "issue_date":sim.issue_date,"lsa":sim.local_service_area}):
+    #         flash("Successfuly Registered")
+    #         return redirect(url_for('.dashboard',user_id =session['uid']))
+    #
+    # flash("Registration failed")
